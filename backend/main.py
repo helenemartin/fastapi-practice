ea91a68c92863722f414a7f272d58a58bea58934
helenemartin <helenemartin13@hotmail.com>
2022-09-03 18:19:55 +0100

    add end points for user auth

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from asyncio import start_unix_server
 import fastapi as _fastapi
 import sqlalchemy.orm as _orm
 import services as _services, schemas as _schemas
@@ -7,6 +8,21 @@ app = _fastapi.FastAPI()
 @app.post("/api/users")
 async def create_user(user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)): 
     db_user = await _services.get_user_by_email(user.email, db)
+
     if db_user:
         raise _fastapi.HTTPException(status_code=400, detail="email already in use")
     return await _services.create_user(user, db)
+
+@app.post("/api/token")
+async def generate_token(form_data: _security.OAuth2PasswordRequestForm = _fastapi.depends(),db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    user = await _services.authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise _fastapi.HTTException(status_code=401, detail="Invalid Credentials")
+
+    return await _services.create_token(user)
+
+@app.get("/api/users/me", response_model=_schemas.User):
+async def get_user(user: _schemas.User = _fastapi.Depends(_services.get_current_user)):
+    return user
+
--- backend/services.py
+++ backend/services.py
@@ -1,6 +1,13 @@
+import fastapi as _fastapi
+import fastapi.security as _security
+import jwt as _jwt
 import sqlalchemy.orm as _orm
 import passlib.hash as _hash
 import database as _database, models as _models, schemas as _schemas
+
+JWT_SECRET = "myjwtsecret"
+
+oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/api/token")
 
 
 def create_database():
@@ -24,3 +31,35 @@ async def create_user(user: _schemas.UserCreate, db: _orm.Session):
     db.commit()
     db.refresh(user_obj)
     return user_obj
+
+async def authenticate_user(email:str, password:str, db:_orm.Session ):
+    user = await get_user_by_email(db=db, email=email)
+
+    if not user:
+        return False
+
+    if not user.verify_password(password):
+        return False
+
+    return user
+
+async def create_token(user: _models.user):
+    user_obj = _schemas.User.from_orm(user)
+
+    token = _jwt.enconde(user_obj.dict(), JWT_SECRET)
+
+    return dict(access_token=token, token_type="bearer")
+
+
+async def get_current_user(
+    db: _orm.Session = _fastapi.Depends(get_db),
+    token: str = _fastapi.Depends(oauth2schema),
+):
+    try:
+        payload = _jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        user = db.query(_models.User).get(payload["id"])
+    except: 
+        raise _fastapi.HTTPException(status_code=401, detail="Invalid Email or Password" )
+    return _schemas.User.from_orm(user)
+
+
